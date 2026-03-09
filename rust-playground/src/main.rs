db04c8f5d99768da5b54eb3a9d7bc485faa676e3
Nhan Nguyen <hoainhaannguyen@gmail.com>
2026-03-09 16:15:47 +0700

    Initial commit

--- rust-playground/src/main.rs
+++ rust-playground/src/main.rs
@@ -2,6 +2,7 @@ mod utils;
 
 use axum::{
     Json, Router,
+    extract::Request,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
@@ -15,6 +16,8 @@ async fn main() {
     // load environment variables from .env file
     dotenv().ok();
     let env = env::var("RUST_ENV").unwrap_or_else(|_| "development".to_string());
+    let host_name = env::var("HOST_NAME").unwrap_or_else(|_| "localhost".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     utils::logger::info(&format!("Starting server in {env} environment"));
 
     // initialize tracing
@@ -28,20 +31,34 @@ async fn main() {
         .route("/users", post(create_user));
 
     // run our app with hyper
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind(&format!("{host_name}:{port}"))
         .await
         .unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
 
     // print a message when the server is ready
-    utils::logger::info("Server is running at http://localhost:3000/");
+    utils::logger::info(&format!("Server is running at {host_name}:{port}"));
 
     axum::serve(listener, app).await.unwrap();
 }
 
 // basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn root(req: Request) -> impl IntoResponse {
+    let host = req
+        .headers()
+        .get("host")
+        .and_then(|v| v.to_str().ok())
+        .unwrap_or("unknown");
+    let method = req.method().clone();
+    let uri = req.uri().clone();
+    let status = StatusCode::OK;
+
+    utils::logger::info(&format!(
+        "[{}] [{}] [{}] [{}]",
+        host, method, uri, status
+    ));
+
+    (status, "Hello, World!")
 }
 
 async fn create_user(
